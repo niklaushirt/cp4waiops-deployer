192cd75be35777626393ff0b0900b2c095540cfa
Niklaus Hirt <niklaushirt@gmail.com>
2022-11-01 16:47:54 +0100

    20221101-1647

---
+++ tools/97_addons/experimental/turbonomic/prime_numbers/prime_numbers.js
@@ -0,0 +1,16 @@
+const { parentPort } = require('worker_threads');
+
+for (let num = 1; num < 10000000; num++) {
+  let isPrime = true;
+  if (num > 1) {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  if (isPrime) {
+    parentPort.postMessage(num);
+  }
+}
---
+++ tools/97_addons/experimental/turbonomic/turbo-metrics-dif/docker/build.sh
@@ -0,0 +1,2 @@
+docker build -t niklaushirt/turbo-dif:1.0.1 .
+docker push niklaushirt/turbo-dif:1.0.1
