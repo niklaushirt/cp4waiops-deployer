0be9781c548d5c3684be160a2d3da61e360d4e55
Niklaus Hirt <nikh@ch.ibm.com>
2023-04-24 14:08:06 +0200

    uninstall

--- tools/99_uninstall/WIPE_COMPLETE.sh
+++ tools/99_uninstall/WIPE_COMPLETE.sh
@@ -1,11 +1,22 @@
+echo "*****************************************************************************************************************************"
+echo " ✅ WIPE REMAINING WAIOPS STUFF"
+echo "*****************************************************************************************************************************"
+echo ""
+echo "  ⏳ INSTALLATION START TIMESTAMP: $(date)"
+echo ""
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete CS CSV"
 oc delete csv --all -n ibm-common-services 
 oc delete subscription --all -n ibm-common-services  
 
+$echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete WAIOPS CSV"
 oc delete csv --all -n cp4waiops 
 oc delete subscription --all -n cp4waiops 
 
 
-
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete PODS"
 oc delete pods -n ibm-common-services --all
 oc delete pods -n cp4waiops --all
 oc delete csv --all -n ibm-common-services
@@ -28,14 +39,23 @@ oc delete pvc -n ibm-common-services --all
 oc delete cm -n cp4waiops --all
 oc delete cm -n ibm-common-services --all
 
-
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete OPERANDREQUESTS"
 oc delete operandrequests.operator.ibm.com -n cp4waiops --all
 oc delete operandrequests.operator.ibm.com -n ibm-common-services --all
 
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete KAFKA Claims"
 oc delete kafkaclaims.shim.bedrock.ibm.com -n cp4waiops --all
 oc delete kafkaclaims.shim.bedrock.ibm.com -n ibm-common-services --all
+
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete OIDC Clients"
 oc delete clients.oidc.security.ibm.com -n cp4waiops --all --force --grace-period=0
 oc delete clients.oidc.security.ibm.com -n ibm-common-services --all
+
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete ConfigMaps"
 oc delete cm -n cp4waiops --all
 oc delete cm -n ibm-common-services --all
 oc delete cm -n cp4waiops --all
@@ -44,7 +64,32 @@ oc delete cm -n cp4waiops --all
 oc delete cm -n ibm-common-services --all
 
 
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace sock-shop"
+oc delete ns sock-shop &
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace robot-shop"
+oc delete ns robot-shop &
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace cp4waiops-demo-ui"
+oc delete ns awx &
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace cp4waiops-demo-ui"
+oc delete ns cp4waiops-demo-ui &
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace cp4waiops-tools"
+oc delete ns cp4waiops-tools &
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace openldap"
+oc delete ns openldap &
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace ibm-common-services "
 oc delete ns ibm-common-services 
+echo "------------------------------------------------------------------------------------------------------------------------------"
+echo " 🧻 Delete Namespace cp4waiops"
 oc delete ns cp4waiops
 
 
