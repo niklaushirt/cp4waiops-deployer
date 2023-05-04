fa36bc252e4d5d3513b5f8cdfef01cb043e2d548
Niklaus Hirt <nikh@ch.ibm.com>
2023-05-04 18:57:35 +0200

    4.1 Zen Service Detection

--- tools/99_uninstall/WIPE_COMPLETE.sh
+++ tools/99_uninstall/WIPE_COMPLETE.sh
@@ -18,27 +18,27 @@ oc delete subscription --all -n cp4waiops
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 🧻 Delete Stuff"
-oc delete pods -n ibm-common-services --all
-oc delete pods -n cp4waiops --all
-oc delete csv --all -n ibm-common-services
-oc delete subscription --all -n ibm-common-services
-oc delete csv --all -n cp4waiops
-oc delete subscription --all -n cp4waiops
-oc delete deployment -n cp4waiops --all
-oc delete deployment -n ibm-common-services --all
-oc delete ss -n ibm-common-services --all
-oc delete statefulset -n ibm-common-services --all
-oc delete statefulset -n cp4waiops --all
-oc delete jobs -n cp4waiops --all
-oc delete jobs -n ibm-common-services --all
-oc delete cm -n cp4waiops --all
-oc delete cm -n ibm-common-services --all
-oc delete secret -n cp4waiops --all
-oc delete secret -n ibm-common-services --all
-oc delete pvc -n cp4waiops --all
-oc delete pvc -n ibm-common-services --all
-oc delete cm -n cp4waiops --all
-oc delete cm -n ibm-common-services --all
+oc delete pods -n ibm-common-services --all &
+oc delete pods -n cp4waiops --all &
+oc delete csv --all & -n ibm-common-services
+oc delete subscription --all & -n ibm-common-services
+oc delete csv --all & -n cp4waiops
+oc delete subscription --all & -n cp4waiops
+oc delete deployment -n cp4waiops --all &
+oc delete deployment -n ibm-common-services --all &
+oc delete ss -n ibm-common-services --all &
+oc delete statefulset -n ibm-common-services --all &
+oc delete statefulset -n cp4waiops --all &
+oc delete jobs -n cp4waiops --all &
+oc delete jobs -n ibm-common-services --all &
+oc delete cm -n cp4waiops --all &
+oc delete cm -n ibm-common-services --all &
+oc delete secret -n cp4waiops --all &
+oc delete secret -n ibm-common-services --all &
+oc delete pvc -n cp4waiops --all &
+oc delete pvc -n ibm-common-services --all &
+oc delete cm -n cp4waiops --all &
+oc delete cm -n ibm-common-services --all &
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 🧻 Delete OPERANDREQUESTS"
