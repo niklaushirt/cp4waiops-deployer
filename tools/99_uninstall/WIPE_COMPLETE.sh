92b3849a9d516ae3446a5992e79485db6a8f5366
Niklaus Hirt <nikh@ch.ibm.com>
2023-05-11 10:35:44 +0200

    wipe

--- tools/99_uninstall/WIPE_COMPLETE.sh
+++ tools/99_uninstall/WIPE_COMPLETE.sh
@@ -135,7 +135,6 @@ oc patch CustomResourceDefinition $(oc get CustomResourceDefinition| grep .ibm.c
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 🧻 Delete ClusterRoleBindings"
-oc delete ClusterRoleBinding cp4waiops-installer-admin &
 oc delete ClusterRoleBinding cp4waiops-demo-ui-admin-crb &                 
 oc delete ClusterRoleBinding cp4waiops-installer-admin &
 oc delete ClusterRoleBinding awx-default &
@@ -151,7 +150,6 @@ oc delete ClusterRoleBinding sre-tunnel-cp4waiops-tunnel-cluster &
 oc delete ClusterRoleBinding sre-tunnel-cp4waiops-tunnel-cluster-api &
 
 
-oc patch ClusterRoleBinding cp4waiops-installer-admin -p '{"metadata":{"finalizers":null}}' --type=merge  
 oc patch ClusterRoleBinding cp4waiops-demo-ui-admin-crb -p '{"metadata":{"finalizers":null}}' --type=merge                       
 oc patch ClusterRoleBinding cp4waiops-installer-admin -p '{"metadata":{"finalizers":null}}' --type=merge 
 oc patch ClusterRoleBinding awx-default -p '{"metadata":{"finalizers":null}}' --type=merge 
@@ -172,6 +170,8 @@ echo " 🧻 Delete IBM CatalogSource"
 oc delete CatalogSource -n openshift-marketplace ibm-operator-catalog
 
 
+oc delete ClusterRoleBinding cp4waiops-installer-admin &
+oc patch ClusterRoleBinding cp4waiops-installer-admin -p '{"metadata":{"finalizers":null}}' --type=merge  
 
 exit 1
 
